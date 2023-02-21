849b6a84f870b0154d2dab416ecb7c4071a74517
Abdul <64811742+abdulRdev@users.noreply.github.com>
2023-02-21 11:18:23 -0500

    Update Employee table script

--- COMP 353 Mini PROJECT/create employee table.sql
+++ COMP 353 Mini PROJECT/create employee table.sql
@@ -1,15 +1,15 @@
 USE ubc353_4;
 Create table Employee(
-employeeID int,
+employeeID int, 
 medicare VARCHAR(12) NOT NULL UNIQUE,
-firstname VARCHAR (25),
-lastname VARCHAR(25),
-birthdate DATE,
+firstName VARCHAR (25),
+lastName VARCHAR(25),
+dateOfBirth DATE,
 phone VARCHAR(15),
 province VARCHAR(30), 
 address VARCHAR(50),
 city VARCHAR(30),
-postal VARCHAR(7),
+postal VARCHAR(7), 
 email VARCHAR(50),
 citzenship VARCHAR(30),
 PRIMARY KEY (employeeID)
