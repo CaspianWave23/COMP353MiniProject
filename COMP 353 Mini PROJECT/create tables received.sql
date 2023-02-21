ab0bf18910256577bfa916ecd76cf414b116bab5
Abdul <64811742+abdulRdev@users.noreply.github.com>
2023-02-21 11:33:38 -0500

    Updated Received table script

--- COMP 353 Mini PROJECT/create tables received.sql
+++ COMP 353 Mini PROJECT/create tables received.sql
@@ -1,11 +1,11 @@
 USE ubc353_4;
 CREATE TABLE Received(
-employeeID int,
-dose_num INT,
-date_received DATE,
-facilityID INT,
 vaccineID INT,
-PRIMARY KEY (employeeID, dose_num),
+employeeID INT,
+doseNum INT,
+dateReceived DATE,
+facilityID INT,
+PRIMARY KEY (employeeID, doseNum),
 FOREIGN KEY (employeeID) REFERENCES Employee(employeeID) ON DELETE CASCADE,
 FOREIGN KEY (facilityID) REFERENCES Facility(facilityID) ON DELETE CASCADE,
 FOREIGN KEY (vaccineID) REFERENCES Vaccine(vaccineID) ON DELETE CASCADE
