7d8a385b426345f8bf9d8cb4c91c260bc452a1f8
Mohamed Allalou <58379314+MohamedAllalou@users.noreply.github.com>
2023-02-21 14:19:27 -0500

    Add files via upload

---
+++ COMP 353 Mini PROJECT/Queries.sql
@@ -0,0 +1,10 @@
+
+USE ubc353_4;
+
+-- 3.i
+-- We are assuming that we are counting both employees and managers.
+SELECT f.facilityProvince, f.facilityName, f.capacity, COUNT(DISTINCT w.employeeID) AS numEmployees, COUNT(DISTINCT m.employeeID) AS numManagers
+FROM Facility f
+LEFT JOIN WorksAt w ON f.facilityID = w.facilityID AND w.endDate IS NULL
+LEFT JOIN Manages m ON f.facilityID = m.facilityID
+GROUP BY f.facilityID
