3c88b8e2259cf8bc6e9487c49cc3706fdaeeaf9e
Abdul <64811742+abdulRdev@users.noreply.github.com>
2023-02-21 11:53:45 -0500

    Create create table Manages.sql

---
+++ COMP 353 Mini PROJECT/create table Manages.sql
@@ -0,0 +1,9 @@
+
+USE ubc353_4;
+CREATE TABLE WorksAt (
+employeeID INT,
+facilityID INT,
+PRIMARY KEY (facilityID),
+FOREIGN KEY (facilityID) REFERENCES Facility(facilityID) ON DELETE CASCADE,
+FOREIGN KEY (employeeID) REFERENCES Employee(employeeID) ON DELETE CASCADE
+);
