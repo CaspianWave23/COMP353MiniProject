f485bee5ad90c2058b8a0882771d70fbfc452a22
Abdul <64811742+abdulRdev@users.noreply.github.com>
2023-02-21 11:23:09 -0500

    Updated infected table creation script

--- COMP 353 Mini PROJECT/create table infected.sql
+++ COMP 353 Mini PROJECT/create table infected.sql
@@ -1,9 +1,9 @@
 USE ubc353_4;
 CREATE TABLE Infected (
-numberInfected int(20),
+infectionNumber int(20),
 infectionDate DATE,
 infectionType VARCHAR(30),
 employeeID int NOT NULL,
-PRIMARY KEY (numberInfected, employeeID),
-FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID) ON DELETE CASCADE
+PRIMARY KEY (infectionNumber, employeeID),
+FOREIGN KEY (employeeID) REFERENCES Employee(employeeID) ON DELETE CASCADE
 );
